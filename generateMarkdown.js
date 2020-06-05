ed553bc2d3acc70d3fb0fed659c919fba67f9f52
sierra <haleyalton93@gmail.com>
2020-06-05 01:21:35 -0700

    mark down generation data calls the beginnings

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,14 @@
 function generateMarkdown(data) {
     return `
   # ${data.title}
+  # ${data.Description}
+  # ${data.TableofContents}
+  # ${answers.Installations}
+  # ${answers.Usage}
+  # ${answers.License}
+  # ${answers.Contributors}
+  # ${answers.Tests}
+  # ${answers.Questions}
   
   `;
 }
